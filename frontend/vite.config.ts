79eb66d2c16d7af88bdbaec676a891f9f7bcfd3e
ab-dauletkhan <ab.dauletkhan@gmail.com>
2025-05-25 07:41:53 +0500

    fix: vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       // string shorthand: /api -> http://localhost:3001/api
       '/api': {
-        target: 'http://localhost:3001', // Your backend server address
+        target: 'https://n-factorial-ai-cup-2025-mi1c.vercel.app:3001', // Your backend server address
         changeOrigin: true,
         // secure: false, // If your backend is not HTTPS
         // rewrite: (path) => path.replace(/^\/api/, '') // Optional: if you don't want /api prefix on backend
